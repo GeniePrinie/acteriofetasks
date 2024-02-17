ef86654335a7fa074d208f9ec20b7d93774f2b5d
GeniePrinie <geniexpn@gmail.com>
2024-02-17 12:22:08 +0100

    Add routing functionality

--- src/RouterPathway.jsx
+++ src/RouterPathway.jsx
@@ -1,3 +1,31 @@
+import React from "react";
+import { Posts } from "./Posts";
+import { Post } from "./Post";
+import { Layout } from "./Layout/Layout";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+function RouteNotFound() {
+  return <div>Page not found</div>;
+}
+
 export function RouterPathway() {
-  return <div>Hello</div>;
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Navigate to="/posts" />} />
+          <Route path="/posts" element={<Layout />}>
+            <Route index element={<Posts />} />
+            <Route path="/posts/:id" element={<Post />} />
+          </Route>
+          <Route path="*" element={<RouteNotFound />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
