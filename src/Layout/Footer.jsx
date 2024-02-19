3308e971b92534287a2a50072fa64e0e30329968
GeniePrinie <geniexpn@gmail.com>
2024-02-19 12:00:49 +0100

    Update body and footer style

--- src/Layout/Footer.jsx
+++ src/Layout/Footer.jsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <footer className="py-3 ps-5 bg-secondary">
+    <footer className="py-3 ps-5 mt-auto bg-secondary">
       &copy;2024 The Posts. Created by Genie Prinyanut
     </footer>
   );
